e9dc1f79a61d0c69dadd5f4975b352de5d8c2320
Shakedflam <shakedflam@gmail.com>
2025-01-05 18:10:54 +0200

    addfollowee for null string

--- Network.java
+++ Network.java
@@ -63,15 +63,12 @@ public class Network {
      *  If any of the two names is not a user in this network,
      *  or if the "follows" addition failed for some reason, returns false. */
     public boolean addFollowee(String name1, String name2) {
-        if (getUser(name1)==null){
-            return false;
-        }
-        if (getUser(name2)==null){
+        if (name1==null || name2==null || getUser(name2)==null || getUser(name1)==null){
             return false;
         }
         User user1= getUser(name1);
         user1.addFollowee(name2);
-        return false;
+        return true;
     }
     
     /** For the user with the given name, recommends another user to follow. The recommended user is
